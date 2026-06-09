5455b7d2c7daf0e9ab8ab7ea018fbabb45cc59c6
Rakhi Hari <microtechworkdeskrakhi@gmail.com>
2026-06-10 01:02:57 +0530

    10-06-2026

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next"
 
 // Contexts
 import { AuthProvider, ProtectedRoute, ClientRoute } from "./contexts/AuthContext";
